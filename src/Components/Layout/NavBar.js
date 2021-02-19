a9a680f1dece6f0b0f89e151b72d92f8a28f0df1
Matheus <mmathias2011@outlook.com>
2021-02-19 16:46:47 -0300

    First commit com a NavBar

---
+++ src/Components/Layout/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top" >
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">Pokedex</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
