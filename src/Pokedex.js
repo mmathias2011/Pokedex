1738343ef4d3c78ee9fe3f112c6124aa791c8dd9
Matheus <mmathias2011@outlook.com>
2021-07-17 19:24:33 -0300

    configuração do grid e pokemon card para testar layout

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import { classExpression } from '@babel/types';
 
-const Pokedex = () => {
+const useStyles = makeStyles({
+    pokedexContainer : {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+    }
+})
+
+const getPokemonCard = () => {
     return(
-        <div>
-            Listagem de pokemon
-        </div>
+        <Grid item xs={12} sm={4}>
+            <Card>
+                <CardContent>hi</CardContent>
+            </Card>
+        </Grid>    
+    )
+}
+const Pokedex = () => {
+     const classes = useStyles();
+    return(
+        <>
+        <AppBar position="static">
+            <Toolbar />
+        </AppBar>
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+            {getPokemonCard()}
+            {getPokemonCard()}
+            {getPokemonCard()}
+            {getPokemonCard()}
+        </Grid>
+        </>
     )
 };
 export default Pokedex
