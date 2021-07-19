87aa382980c0a50e6c098725db9698ccda7e07c8
Matheus <mmathias2011@outlook.com>
2021-07-19 12:11:30 -0300

    Finalizando projeto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Router } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'react-history'
 
 const history = createBrowserHistory();
 
