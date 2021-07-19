681b8daaaa2ac9eb2cf472018cde1004cb9a4691
Matheus <mmathias2011@outlook.com>
2021-07-19 11:26:28 -0300

    Adicionando useEffect para a chamada api, criando um objeto que vai ser setado dentro do state setPokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,39 +1,73 @@
-import React from 'react'
-import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
-import { classExpression } from '@babel/types';
+import React, { useState, useEffect} from "react";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardContent,
+  CircularProgress,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles({
-    pokedexContainer : {
-        paddingTop: "20px",
-        paddingLeft: "50px",
-        paddingRight: "50px",
-    }
-})
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
 
-const getPokemonCard = () => {
-    return(
-        <Grid item xs={12} sm={4}>
-            <Card>
-                <CardContent>hi</CardContent>
-            </Card>
-        </Grid>    
-    )
-}
+
 const Pokedex = () => {
-     const classes = useStyles();
-    return(
-        <>
-        <AppBar position="static">
-            <Toolbar />
-        </AppBar>
-        <Grid container spacing={2} className={classes.pokedexContainer}>
-            {getPokemonCard()}
-            {getPokemonCard()}
-            {getPokemonCard()}
-            {getPokemonCard()}
-        </Grid>
-        </>
-    )
+  const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  }, []);
+
+  const getPokemonCard = (pokemonId) => {
+    console.log(pokemonData[`${pokemonId}`])
+  return (
+    <Grid item xs={12} sm={4} key={pokemonId}>
+      <Card>
+        <CardContent>hi</CardContent>
+      </Card>
+    </Grid>
+  );
 };
-export default Pokedex
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      {pokemonData ? (
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map((pokemonId) =>
+            getPokemonCard(pokemonId)
+          )}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+};
+export default Pokedex;
